5031b30317bcb1e684caa3fb18f010aca0cd04cd
Thanh nx <thanhnx@Thanhs-MacBook-Pro.local>
2020-10-26 23:51:24 +0700

    animation with js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,24 @@
 // gsap.from("img", {x: 800, y: 400, duration: 3, rotation: 360,  repeat: -1, yoyo: true});
 
-let path = document.querySelectorAll('#logo path');
-console.log(path)
-for (let i = 0; i < path.length; i++) {
-    console.log(`path ${i}: ${path[i].getTotalLength()}`)
-}
+// let path = document.querySelectorAll('#logo path');
+// console.log(path)
+// for (let i = 0; i < path.length; i++) {
+//     console.log(`path ${i}: ${path[i].getTotalLength()}`)
 
-gsap.timeline()
-    .from('#input', { opacity: 1, duration: 0.5 })
-    .from('#text', { y: 160, duration: 1 })
-    .from('#box', {opacity: 0})
-    .from('#icon', { opacity: 0, scale: 2, ease: "back.out(1.7)", x: 5 })
-
-
-// GSDevTools.create();
-
-// var tl = gsap.timeline({onUpdate:updateSlider, defaults: {duration: 1}}),
-//     circle = document.getElementById("circle");
-
-// tl.to(circle, {morphSVG:"#hippo"}, "+=1")
-//   .to(circle, {morphSVG:"#star"}, "+=1")
-//   .to(circle, {morphSVG:"#elephant"}, "+=1")
-//   .to(circle, {morphSVG:circle}, "+=1");
-
-
-
-
-
-
-
-
-// /* controls */
-
-// $("#slider").slider({
-//   range: false,
-//   min: 0,
-//   max: 1,
-//   step:.001,
-//   slide: function ( event, ui ) {
-//     tl.progress( ui.value ).pause();
-//   },
-//   stop: function () {
-//     tl.play();
-//   }
-// }); 
-
-// function updateSlider() {
-//   $("#slider").slider("value", tl.progress());
-// } 
+document.addEventListener('DOMContentLoaded', () => {
+    function animateSgv (id, delay, delayIncrement){
+        const logo = document.getElementById(id);
+        const logoPaths = document.querySelectorAll(`#${id} path`);
+        delay = delay;
+        for(let i = 0; i < logoPaths.length;i++){
+            logoPaths[i].style.strokeDasharray  = logoPaths[i].getTotalLength();
+            logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+            logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
+            delay+=delayIncrement;
+            console.log(delay)
+        }
+        logo.style.animation = `fill 0.5s ease forwards ${delay}s`;
+    }
+// Set the id of SVG, delay time in seconds to start animation and delay between each animation
+    animateSgv('logo', 0, 0.8) 
+}, false);
